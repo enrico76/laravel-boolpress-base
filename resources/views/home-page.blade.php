14321a0cda54868e900e1580edb8da9a89ac71ee
enrico76 <enricodip@gmail.com>
2019-10-17 19:12:00 +0200

    add scss, controller, first route,view homepage + layout

---
+++ resources/views/home-page.blade.php
@@ -0,0 +1,6 @@
+@extends('layout.layout-base')
+
+@section('content')
+  hello world
+
+@endsection
---
+++ resources/views/layout/layout-base.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Bollpress</title>
+
+        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+
+    </head>
+    <body>
+      <div class="container">
+        <header>
+          <h1>boolpress</h1>
+        </header>
+        <main>
+          @yield('content')
+        </main>
+        <footer>
+          <h4>Myfooter</h4>
+        </footer>
+
+      </div>
+
+    </body>
+</html>
