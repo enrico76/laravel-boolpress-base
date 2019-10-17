dc42eebd2097e9f84f3235b6e63bebec4758e8f4
enrico76 <enricodip@gmail.com>
2019-10-17 20:29:37 +0200

    add route for create and store post

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,5 @@
 Route::get('/', 'MyController@index')->name('home-page');
 Route::get('/category/{id}', 'MyController@showCategory') -> name('categories');
 Route::get('/post/{id}', 'MyController@showPost') -> name('posts');
+Route::get('admin/post/create', 'MyController@create') -> name('create.post');
+Route::post('admin/post/store', 'MyController@store') -> name('store.post');
